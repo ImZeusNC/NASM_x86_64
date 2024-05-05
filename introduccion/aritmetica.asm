b6d4769d42d7c33fd4014a4b7618e45e5b15ce21
Zeus <lucasmedinaavila@protonmail.com>
2024-05-05 18:03:47 -0400

    Cree aritmetica.asm

---
+++ introduccion/aritmetica.asm
@@ -0,0 +1,26 @@
+; Instrucciones con operaciones aritmeticas entre los operandos
+section .text
+    global _start
+
+_start:
+    ;suma aritmetica
+    mov al, 5    ;al = 5
+    mov bl, 4    ;bl = 4
+    add al, bl   ;al = al + bl
+    ;resta aritmetica
+    mov cl, 7    ;cl = 7
+    mov dl, 3    ;dl = 3
+    sub cl, dl   ;cl = cl - dl
+    ;multiplicacion aritmetica
+    mov cl, 7    ; cl = 7
+    mov dl, 2    ; dl = 2
+    imul cl      ; cl = cl * dl
+    ;division aritmetica
+    mov al, 15   ; al = 15
+    mov bl, 3    ; bl = 3
+    idiv al      ; al = al / bl
+    ;residuo se guarda en ah
+    ;finaliza el programa
+    mov rax, 60
+    mov rdi, 0
+    syscall
